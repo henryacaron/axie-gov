f0a4322807946a85681b5bbd586282e8129531f4
henrycaron <57197754+henrycaron@users.noreply.github.com>
2021-12-22 17:27:29 -0500

    reconfigured

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -80,33 +80,15 @@ export default function Home() {
         </div>
         <Button onClick={disconnect}>Disconnect</Button>
        </Navbar>
-      {/* <Form setChoice = {setChoice()} choice = {choice}/>  */}
-      <form className = "d-flex flex-column bg-light width-100 justify-content-center align-items-center">
-        <h3>True or false?</h3>
-
-          <Container className = "d-flex flex-row justify-content-around">
-            <Button className = {choice ? "bg-btn btn-primary" : "btn btn-light"} onClick={(e) => setChoice(true)}
->
-            <h5>True</h5>
-            
-            </Button>
-            <Button  onClick={(e) => setChoice(false)} className = {choice == false ? "bg-btn btn-primary" : "btn btn-light"} >
-              <h5>False</h5>
-             
-            </Button>
-          </Container>
-          <Button className = "mt-5" onClick={() => sign(choice)}>Confirm Choice and Sign</Button>
-
-      </form>
-  
-      </Container>}
+      <Form setChoice = {setChoice} choice = {choice}/>  
+      <Button className = "mt-5" onClick={() => sign(choice)}>Confirm Choice and Sign</Button>  
+    </Container>}
+    
     {!active &&
-        <Container className="d-flex w-100 align-items-center justify-content-center h-100">
+        <Navbar className="d-flex align-items-center justify-content-center h-100">
           <Button onClick={connect} className = "mr-10">Connect to Metamask</Button>
           {/* <span>Not connected</span> */}
-        </Container>}
-      
-      {/* <Table/> */}
+        </Navbar>}
     </Container>
   )
 }
