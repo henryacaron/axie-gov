5ab95004bfba9748320d6c6aabb4db39691aad66
Henry Caron <henryacaron@gmail.com>
2021-12-21 20:21:48 -0500

    table added
    built basic buttons
    
    signing works
    
    basic styling
    
    reconfigured
    
    basic backend
    
    send messages to backend
    
    can retrieve previous proposals
    
    set up table
    
    hid api key
    
    removed keys
    
    modify gitignore
    
    deleted backend .env file

---
+++ frontend/pages/index.js
@@ -0,0 +1,84 @@
+import React, {useState, useEffect} from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import {Container, Accordion, Button} from "react-bootstrap"
+import styles from "../styles/Home.module.css";
+import { injected } from "../components/wallet/Connectors"
+import { useWeb3React } from "@web3-react/core";
+import Table from "../components/Table";
+import Question from "../components/Question";
+import Header from "../components/Header";
+import PrevProposals from "../components/PrevProposals";
+
+var axios = require("axios").default;
+
+export default function Home() {
+  const { active, account, library } = useWeb3React()
+  const [playerData, setPlayerData] = useState(undefined)
+  const [choices, setChoices] = useState({})
+  const qData = [
+    {skillName : "Aqua Vitality", class : "Aquatic", Shield: 40, Attack: 80, Desc: ""},
+    {skillName : "Aquaponics", class : "Aquatic", Shield: 40, Attack: 80},
+
+  ]
+  
+  async function sign(message) {
+    let msgString = JSON.stringify(message);
+    const signer = library.getSigner()
+    const sig = await signer.signMessage(msgString);
+    
+    axios.post('http://localhost:3001/submit/', {account: account, msg: msgString, sig : sig})
+    .then((res) => {
+      if(res.type == 'error'){
+        alert(`Error: ${res.data.message}`)
+      } else alert(res.data.message)
+    })
+      .catch((err) => {
+          console.log(err.response);
+      });
+  }
+  
+  const API_KEY = process.env.REACT_APP_API_KEY
+
+  function handleChoiceChange(skill, attr, change){
+    setChoices({...choices, [skill] : {...choices[skill], [attr] : change}});
+  }
+  
+  return (
+    <Container className = "d-flex flex-column justify-content-center">
+      <Header playerData = {playerData} setPlayerData = {setPlayerData}/>
+      <br/>
+      {active && 
+        <Container className = "d-flex flex-column justify-content-center">
+          <Accordion className = "h-100 w-80" variant="dark">
+            {qData.map((q, idx) => (<
+              Accordion.Item key = {idx} eventKey = {idx}>
+                <Accordion.Header>{q.skillName}</Accordion.Header>
+                <Accordion.Body>
+                  <Question 
+                    qData = {q} 
+                    playerData = {playerData} 
+                    choice = {choices[q.skillName]} 
+                    setChoice = {handleChoiceChange}
+                    />
+                </Accordion.Body>
+              </Accordion.Item>))}
+          </Accordion>
+          <hr/>
+          
+          <Button disabled = {!(playerData)} 
+                className = "mt-5" 
+                onClick={() => {
+                  qData.proposal = choices;
+                  sign(choices);
+                }}>
+          Confirm Proposals and Sign
+        </Button>
+          <hr/>
+          
+          {/*<PrevProposals qData = {qData}/> */} 
+        </Container>
+      }
+    </Container>
+  )
+}
